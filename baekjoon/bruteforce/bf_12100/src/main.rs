f2f0db5dcc7a8d1cc2c55459546c5cff79574db6
정경빈 <raspy1108@gmail.com>
2020-12-18 18:27:17 +0900

    오류 수정해야함. 먼가 병합이 잘못됨

--- baekjoon/bruteforce/bf_12100/src/main.rs
+++ baekjoon/bruteforce/bf_12100/src/main.rs
@@ -2,6 +2,8 @@ use std::str::{FromStr, SplitWhitespace};
 use std::fmt::Debug;
 use std::io::{stdin, Read};
 use std::cmp::max;
+use std::mem::transmute;
+use crate::direction::{UP, LEFT, RIGHT};
 
 trait Parser {
     fn read<T,E>(&mut self) -> T
@@ -16,20 +18,48 @@ impl<'a> Parser for SplitWhitespace<'a> {
     }
 }
 
-fn merge_line(mut v: Vec<u16>) -> (Vec<u16>, u16) {
+#[derive(Copy, Clone)]
+enum direction {
+    UP = 0,
+    DOWN = 1,
+    LEFT = 2,
+    RIGHT = 3
+}
+
+fn merge_line(v: &mut Vec<Vec<u16>>, idx: usize, d: direction){
     let mut i = 0;
     let mut j = i+1;
     let mut m = 0;
-    while i < v.len() && j < v.len() {
-        if v[j] != 0 {
-            if v[i] == v[j] {
-                v[i] += v[j];
-                v[j] = 0;
+    let len = v.len();
+    let mut copy: Vec<u16>;
+    match d {
+        direction::UP => {
+            copy = v.iter().map(|x| x[idx]).collect()
+        },
+        direction::DOWN => {
+            copy = v.iter().rev().map(|x| x[idx]).collect();
+        },
+        direction::LEFT => {
+            copy = v[i].iter().map(|x| *x).collect();
+        },
+        direction::RIGHT => {
+            copy = v[i].iter().rev().map(|x| * x).collect();
+        }
+    }
+    while i < copy.len() && j < copy.len() {
+        if i == j {
+            j += 1;
+            continue
+        }
+        if copy[j] != 0 {
+            if copy[i] == copy[j] {
+                copy[i] += copy[j];
+                copy[j] = 0;
                 i += 1;
                 j += 1;
-            } else if v[i] == 0 {
-                v[i] = v[j];
-                v[j] = 0;
+            } else if copy[i] == 0 {
+                copy[i] = copy[j];
+                copy[j] = 0;
                 j += 1;
             } else {
                 i+= 1;
@@ -38,17 +68,70 @@ fn merge_line(mut v: Vec<u16>) -> (Vec<u16>, u16) {
             j += 1;
         }
     }
-    return (v, 0)
+    match d {
+        direction::UP => {
+            for i in 0..v.len() {
+                v[i][idx] = copy[i];
+            }
+        },
+        direction::DOWN => {
+            for i in 0..v.len() {
+                v[(len -1 ) - i][idx] = copy[i];
+            }
+        },
+        direction::LEFT => {
+            for i in 0..v.len() {
+                v[idx][i] = copy[i];
+            }
+        },
+        direction::RIGHT => {
+            for i in 0..v.len() {
+                v[idx][(len-1) - i] = copy[i];
+            }
+        }
+    }
+}
+
+fn permutation(v: &mut Vec<Vec<u16>>, d: direction) {
+    for i in 0..v.len() {
+        merge_line(v, i, d);
+    }
+}
+
+fn search(v: &mut Vec<Vec<u16>>, n: u8) -> u16{
+    if n == 5 {
+        let mut m = 0;
+        for i in v {
+            for j in i {
+                m = max(m,*j);
+            }
+        }
+        m
+    } else {
+        let mut m = 0;
+        for i in 0..4 {
+            let temp = v.clone();
+            permutation(v, unsafe{transmute(i as i8) });
+            m = max(search(v, n + 1),m);
+            v.clone_from_slice(&temp[..]);
+        }
+        m
+    }
 }
 fn main() {
-    /*
     let mut s = String::new();
 
     stdin().read_to_string(&mut s).expect("read error");
     let mut iter = s.split_whitespace();
 
-     */
-    println!("{:?}", merge_line(vec![2,0,2,4,4]));
-    println!("{:?}", merge_line(vec![2,0,2,4,8]));
-    println!("{:?}", merge_line(vec![2,2,4,4,8]));
+    let n : u32 = iter.read();
+    let mut v = Vec::new();
+    for i in 0..n {
+        let mut temp = Vec::new();
+        for i in 0..n {
+            temp.push(iter.read());
+        }
+        v.push(temp);
+    }
+    println!("{}", search(&mut v,0));
 }
